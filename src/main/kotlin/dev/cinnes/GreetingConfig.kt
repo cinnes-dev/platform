b22ddfb4219a8dd5ee88e847ae42f2492a6931f1
Callum Innes <callum.innes@starlingbank.com>
2024-07-17 20:42:16 +0100

    init

---
+++ src/main/kotlin/dev/cinnes/GreetingConfig.kt
@@ -0,0 +1,13 @@
+package dev.cinnes
+
+import io.smallrye.config.ConfigMapping
+import io.smallrye.config.WithName
+
+
+@ConfigMapping(prefix = "greeting")
+interface GreetingConfig {
+
+    @WithName("message")
+    fun message(): String?
+
+}
---
+++ src/test/kotlin/dev/cinnes/ExampleResourceIT.kt
@@ -0,0 +1,6 @@
+package dev.cinnes
+
+import io.quarkus.test.junit.QuarkusIntegrationTest
+
+@QuarkusIntegrationTest
+class ExampleResourceIT : ExampleResourceTest()
