5597be1cb95ad60241b9553d9bd9ec82d0155db1
Callum Innes <me@cinnes.dev>
2024-07-18 02:21:19 +0100

    Add more dependencies for cloudbuild

--- terraform/cloudbuild.tf
+++ terraform/cloudbuild.tf
@@ -12,7 +12,9 @@ resource "google_cloudbuild_trigger" "service-account-trigger" {
   filename        = "cloudbuild.yaml"
   depends_on = [
     google_project_iam_member.act_as,
-    google_project_iam_member.logs_writer
+    google_project_iam_member.logs_writer,
+    google_project_iam_member.storage_admin,
+    google_project_iam_member.artifact_registry_admin
   ]
 
   include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
