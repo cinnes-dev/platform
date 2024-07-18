35e2417c82a251012bca4f1ba4fd271b747adc7e
Callum Innes <me@cinnes.dev>
2024-07-18 15:08:33 +0100

    move args to tf

--- terraform/cloudrun.tf
+++ terraform/cloudrun.tf
@@ -1,13 +1,19 @@
-resource "google_cloud_run_service" "platform" {
+resource "google_cloud_run_v2_service" "platform" {
   name     = "platform"
   location = "us-central1"
   project  = var.project
 
   template {
-    spec {
-      containers {
-        image = "us-docker.pkg.dev/cloudrun/container/hello"
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+
+      resources {
+        limits = {
+          cpu = "1"
+          memory = "512Mi"
+        }
       }
     }
+    service_account = google_service_account.cloudbuild_service_account.email
   }
 }
