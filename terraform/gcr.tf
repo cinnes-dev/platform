344ed83bb49e60aa00149b69667eedb1e573b091
Callum Innes <me@cinnes.dev>
2024-07-20 17:52:13 +0100

    Fix image ref

--- terraform/gcr.tf
+++ terraform/gcr.tf
@@ -12,5 +12,5 @@ resource "google_artifact_registry_repository" "platform" {
 data "google_artifact_registry_docker_image" "myimage" {
   location      = google_artifact_registry_repository.platform.location
   repository_id = google_artifact_registry_repository.platform.repository_id
-  image_name = "platform:latest"
+  image_name = var.platform-repo-name
 }
