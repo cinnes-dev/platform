e5c99858cbf114efa666b850cf4e772fe270b10b
Callum Innes <me@cinnes.dev>
2024-07-20 19:46:24 +0100

    tweak log bucket name

--- terraform/cloudstorage.tf
+++ terraform/cloudstorage.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "cloudbuild_logs" {
-  name     = "cloudbuild-logs"
+  name     = "cinnes-dev-platform-cloudbuild-logs"
   location = "EU"
   force_destroy = true
   public_access_prevention = "enforced"
