b660008c2cfe73e79c26fb0e2dae516ecdca0a80
Callum Innes <me@cinnes.dev>
2024-07-20 19:47:07 +0100

    Add note about unique cloud storage bucket name

--- terraform/cloudstorage.tf
+++ terraform/cloudstorage.tf
@@ -1,4 +1,5 @@
 resource "google_storage_bucket" "cloudbuild_logs" {
+  # Unique across all cloud storage users
   name     = "cinnes-dev-platform-cloudbuild-logs"
   location = "EU"
   force_destroy = true
