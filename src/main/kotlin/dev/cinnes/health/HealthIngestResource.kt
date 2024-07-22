7559511768c0dd46589f8e83092f5d532628d05c
Callum Innes <me@cinnes.dev>
2024-07-22 21:07:57 +0100

    log health ingest

---
+++ src/main/kotlin/dev/cinnes/health/HealthIngestResource.kt
@@ -0,0 +1,18 @@
+package dev.cinnes.health
+
+import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.POST
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.core.MediaType.APPLICATION_JSON
+import org.jboss.logging.Logger
+
+@Path("/health/ingest")
+class HealthIngestResource {
+
+    val log: Logger = Logger.getLogger(this.javaClass.name);
+
+    @POST
+    @Consumes(APPLICATION_JSON)
+    fun ingest(body: String): Unit = log.info(body)
+
+}
